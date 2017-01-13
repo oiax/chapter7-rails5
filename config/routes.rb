aba49b1b0bbf6037be1d5da2f1ed802b760ddd44
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-01-13 22:02:06 +0900

    [deprecation] Using a dynamic :action segment in a route is deprecated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "top#index"
   get "about" => "top#about", as: "about"
-  get "lesson/:action(/:name)" => "lesson"
+
+  1.upto(19) do |n|
+    get "lesson/step#{n}(/:name)" => "lesson#step#{n}"
+  end
 
   resources :members do
     collection { get "search" }
