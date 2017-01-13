ebd3862a09d3584fccb58235360354be2733f1b0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-01-13 22:07:16 +0900

    [deprecation] ActiveRecord::Base.raise_in_transactional_callbacks= is deprecated

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ module Asagao
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.action_controller.permit_all_parameters = true
   end
 end
